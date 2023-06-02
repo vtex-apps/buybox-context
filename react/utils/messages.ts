612b905a8dd2cf54944bf70fd251c417a360afd4
sheilagomes <sheilagomes1@gmail.com>
2023-06-02 11:12:10 -0300

    Fix store message standardization

--- react/utils/messages.ts
+++ react/utils/messages.ts
@@ -5,27 +5,27 @@ export const messages: {
   [name: string]: MessageDescriptor
 } = defineMessages({
   title: {
-    id: '"admin/buybox-context.title',
+    id: 'store/buybox-context.title',
   },
   sortStrategyTitle: {
-    id: '"admin/buybox-context.sortStrategy-title',
+    id: 'store/buybox-context.sortStrategy-title',
   },
   sortStrategyDescription: {
-    id: '"admin/buybox-context.sortStrategy-description',
+    id: 'store/buybox-context.sortStrategy-description',
   },
   triggerCepChangeEventTitle: {
-    id: '"admin/buybox-context.triggerCepChangeEvent-title',
+    id: 'store/buybox-context.triggerCepChangeEvent-title',
   },
   triggerCepChangeEventDescription: {
-    id: '"admin/buybox-context.triggerCepChangeEvent-description',
+    id: 'store/buybox-context.triggerCepChangeEvent-description',
   },
   expressionTitle: {
-    id: '"admin/buybox-context.expression-title',
+    id: 'store/buybox-context.expression-title',
   },
   expressionDescription: {
-    id: '"admin/buybox-context.expression-description',
+    id: 'store/buybox-context.expression-description',
   },
   protocolError: {
-    id: '"admin/buybox-context.protocol-error',
+    id: 'store/buybox-context.protocol-error',
   },
 })
